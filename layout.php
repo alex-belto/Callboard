38c38acb29f4daf96044a1db33384a4d2b4127bb
alex-belto <58671690+alex-belto@users.noreply.github.com>
2020-09-30 20:39:03 +0300

    Add files via upload

---
+++ layout.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Callboard</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="">
+    </head>
+
+    <body>
+        <main>
+            <?= $content ?>
+        </main>
+    </body>
+</html>
