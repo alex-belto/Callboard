de3da9d18b9c91287c947caefcf405540f0887ca
alex-belto <58671690+alex-belto@users.noreply.github.com>
2020-09-30 22:52:30 +0300

    added header

--- layout.php
+++ layout.php
@@ -7,6 +7,14 @@
     </head>
 
     <body>
+        <header>
+            <ul>
+                <li><a href="Callboard"></a></li>
+                <li><a href="Приютить"></a></li>
+                <li><a href="Потеряшки"></a></li>
+                <li><a href="Контакты"></a></li>
+            </ul>
+        </header>
         <main>
             <?= $content ?>
         </main>
