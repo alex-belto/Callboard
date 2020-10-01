c2b29026eff50b03e2d07190c24be7cbc5403e54
alex-belto <58671690+alex-belto@users.noreply.github.com>
2020-10-01 18:21:36 +0300

    added include.php

---
+++ include.php
@@ -0,0 +1,12 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
+$host='localhost';
+$user='root';
+$password='root';
+$dbname='callboard';
+
+$link= mysqli_connect($host, $user, $password, $dbname);
+mysqli_query($link, "SET NAMES 'utf-8'");
