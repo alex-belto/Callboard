1939da78aff37ccbaf7e00222404e7dbd8175464
alex-belto <58671690+alex-belto@users.noreply.github.com>
2020-10-04 19:26:32 +0300

    nsp

--- layout.php
+++ layout.php
@@ -13,6 +13,7 @@
                 <li><a href="shelter.php">Приютить</a></li>
                 <li><a href="lost.php">Потеряшки</a></li>
                 <li><a href="">Контакты</a></li>
+                <li><a href="login.php">Автоотзация</a></li>
                 <li><a href="registration.php">Регистрация</a></li>
             </ul>
         </header>
--- registration.php
+++ registration.php
@@ -9,18 +9,26 @@ $form_content="
     <p>Введите ваш email:</p>
     <input type='email' name='email'>
     <p>Введите номер вашего телефона:</p>
-    <input type='tel' name='phone'><br><br>
+    <input type='tel' name='phone'>
+    <p>Введите желаемый логин:</p>
+    <input type='text' name='login'>
+    <p>Введите желаемый пароль:</p>
+    <input type='password' name='password'><br><br>
     <input type='submit' name='submit' value='Отправить'> 
 </form>";
 
 function registration($link){
 
-    if(isset($_POST['name'])and isset($_POST['email']) and isset($_POST['phone'])){
+    if(isset($_POST['name'])and isset($_POST['email']) and isset($_POST['phone'])
+    and isset($_POST['login'])and isset($_POST['password'])){
         $name=$_POST['name'];
         $email=$_POST['email'];
         $phone=$_POST['phone'];
+        $login =$_POST['login'];
+        $password =$_POST['password'];
 
-        $query="INSERT INTO users (name, email, phone_numb, access) VALUE ('$name', '$email', '$phone', '1')";
+        $query="INSERT INTO users (name, email, phone_numb, login, password, access)
+         VALUE ('$name', '$email', '$phone', '$login', '$password', '1')";
         mysqli_query($link, $query) or die(mysqli_error($link));
 
     }
