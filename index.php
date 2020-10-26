6b995f167ad1d4c41f37804e754bb8c1679692f7
alex-belto <58671690+alex-belto@users.noreply.github.com>
2020-10-26 17:56:17 +0200

    camelsBacks and $ = a

--- index.php
+++ index.php
@@ -1,80 +1,80 @@
 <?php
 include 'include.php';
 include 'profile.php';
-$form_content='';
+$formContent = '';
 
 function jump($link){
 
-    $block_time= time()+3600*24;// сутки блокировки
-    $freeTime = date("F j, Y, H:i", $block_time);//дата снятия блокировки
+    $blockTime = time()+3600*24;// сутки блокировки
+    $freeTime = date("F j, Y, H:i", $blockTime);//дата снятия блокировки
     $id = $_SESSION['id'];
     
-    $query= "SELECT * FROM users WHERE id = '$id'";// достаю существующий block_time
+    $query = "SELECT * FROM users WHERE id = '$id'";// достаю существующий block_time
        $result = mysqli_fetch_assoc(mysqli_query($link, $query));
        $limit = $result['block_time'];//существующий block_time
 
-    $query="SELECT position, user_id,  id FROM advert WHERE position = (SELECT MAX(position) FROM advert )"; //Достаю максимальную позицию
+    $query = "SELECT position, user_id,  id FROM advert WHERE position = (SELECT MAX(position) FROM advert )"; //Достаю максимальную позицию
     $result = mysqli_fetch_assoc(mysqli_query($link, $query));
     $topPosition = $result['position']; //текущее максимальное положение
-    $topId = $result['id']+ 1;//id максимальной позиции
+    
     
     
     if(isset($_GET['position']) AND isset($_GET['ad_id'])){
-        $positionUpdate= $_GET['position'];// id записи которая апдейтится
-        $ad_id = $_GET['ad_id']; 
+        $positionUpdate = $_GET['position'];// id записи которая апдейтится
+        $adId = $_GET['ad_id']; 
         if(time() > $limit){//наличие блокировки в данный момент
-            $query= "SELECT id, position FROM advert WHERE position  BETWEEN '$positionUpdate' AND '$topPosition'";
+            $query = "SELECT id, position FROM advert WHERE position  BETWEEN '$positionUpdate' AND '$topPosition'";
             $result =  mysqli_query($link, $query) or die(mysqli_error($link));
-            for($arr=[]; $step= mysqli_fetch_assoc($result); $arr[] = $step);
+            for($arr = []; $step = mysqli_fetch_assoc($result); $arr[] = $step);
         foreach($arr as $value){
             $position = $value['position'];
-            $for_id = $value['id'];
+            $forId = $value['id'];
             $downgradePosition =  $position - 1;
-            $query = "UPDATE advert SET position = '$downgradePosition' WHERE id = '$for_id'"; //понижаю все позиции на 1
+            $query = "UPDATE advert SET position = '$downgradePosition' WHERE id = '$forId'"; //понижаю все позиции на 1
             mysqli_query($link, $query) or die(mysqli_error($link));
         }
         
-        $query="UPDATE advert SET position = '$topPosition' WHERE id = '$ad_id' ";// апдейтим нужную позицию
+        $query="UPDATE advert SET position = '$topPosition' WHERE id = '$adId' ";// апдейтим нужную позицию
         mysqli_query($link, $query) or die(mysqli_error($link));
         
-        $query="UPDATE users SET block_time='$block_time' WHERE id = '$id' "; // блок на аккаунт пользователя - 24 часа
+        $query="UPDATE users SET block_time = '$blockTime' WHERE id = '$id' "; // блок на аккаунт пользователя - 24 часа
         mysqli_query($link, $query) or die(mysqli_error($link));
 
-        $_SESSION['message']="Ваша запись поднята в топ, следующая попытка возможна $freeTime";
+        $_SESSION['message'] = "Ваша запись поднята в топ, следующая попытка возможна $freeTime";
 
         }else{
-            $_SESSION['message']= "Ваш лемит поднятий в топ на сегодня привышен, следующая попытка возможна $freeTime";
+            $_SESSION['message'] = "Ваш лемит поднятий в топ на сегодня привышен, следующая попытка возможна $freeTime";
         }
     }
 }
 
 function getList($link){
 
-    $user_id = $_SESSION['id'];// id авторизированого пользователя
+    $userId = $_SESSION['id'];// id авторизированого пользователя
 
     if(isset($_GET['page'])){
         $page = $_GET['page'];
     }else{
-        $page=1;
+        $page = 1;
     }
 
-    $notesOnPage = 2;
-    $referencePoint= ($page - 1) * $notesOnPage;
+    $notesOnPage = 4;
+    $referencePoint = ($page - 1) * $notesOnPage;
 
-    $query="SELECT text, users.id,  name, position, advert.id as ad_id, phone_numb, email FROM users
+    $query = "SELECT text, users.id,  name, position, advert.id as ad_id, phone_numb, email FROM users
     RIGHT JOIN advert ON users.id = advert.user_id ORDER BY position DESC LIMIT $referencePoint, $notesOnPage";
     $result = mysqli_query($link, $query) or die(mysqli_error($link));
-    $content='';
-    for($arr=[]; $step = mysqli_fetch_assoc($result); $arr[]=$step);
+    $content = '';
+    for($arr = []; $step = mysqli_fetch_assoc($result); $arr[] = $step);
 
     foreach($arr as $value){
         $text = $value['text'];
         $name = $value['name'];
         $phone_numb = $value['phone_numb'];
         $email = $value['email'];
-        $ad_position = $value['position'];
-        $ad_user_id= $value['id'];
-        $ad_id = $value['ad_id'];
+        $adPosition = $value['position'];
+        $adUserId = $value['id'];
+        $adId = $value['ad_id'];
         
 
         
@@ -92,28 +92,28 @@ function getList($link){
                 <tr>
                     <td>$email</td>
                 </tr>";
-            if($user_id == $ad_user_id AND $user_id != 5){
+            if($userId == $adUserId AND $userId != 5){
                 
                 $content.="<tr>
                                     <td><form method='GET'>
-                                    <button><a href=\"index.php?position=$ad_position&ad_id=$ad_id\">Top up</a></button>
+                                    <button><a href=\"index.php?position=$adPosition&ad_id=$adId\">Top up</a></button>
                                     </form></td>
                                 </tr>";
             }
         $content.="</table><br>";
     }
 
-    $query= "SELECT COUNT(*) as count FROM advert";//Подсчет кол-ва записей
-    $count=mysqli_fetch_assoc(mysqli_query($link, $query))['count'];
-    $numbsOfPage= ceil($count/$notesOnPage);//кол-во страниц, число записей делим на желаемое на страницу
+    $query = "SELECT COUNT(*) as count FROM advert";//Подсчет кол-ва записей
+    $count = mysqli_fetch_assoc(mysqli_query($link, $query))['count'];
+    $numbsOfPage = ceil($count/$notesOnPage);//кол-во страниц, число записей делим на желаемое на страницу
     //$content.= $numbsOfPage;
 
     if($page != 1){
         $previous = $page-1;
-        $content.= "<a href=\"?page=$previous\"><<<</a>";//стрелки
+        $content.= "<a href=\"?page = $previous\"><<<</a>";//стрелки
     }
 
-    for($i=1; $i <= $numbsOfPage; $i++){
+    for($i = 1; $i <= $numbsOfPage; $i++){
         $content.="<a href=\"?page=$i\">$i</a>";
     }
 
--- layout.php
+++ layout.php
@@ -2,29 +2,29 @@
 <html>
     <head>
         <title>Callboard</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="">
+        <meta charset = "utf-8">
+        <link rel = "stylesheet" type = "text/css" href = "">
     </head>
 
     <body>
         <header>
             <ul>
-                <li><a href="index.php">Callboard</a></li>
-                <li><a href="shelter.php">Приютить</a></li>
-                <li><a href="lost.php">Потеряшки</a></li>
-                <li><a href="">Контакты</a></li>
+                <li><a href = "index.php">Callboard</a></li>
+                <li><a href = "shelter.php">Приютить</a></li>
+                <li><a href = "lost.php">Потеряшки</a></li>
+                <li><a href = "">Контакты</a></li>
                 <?php
                 if(!empty($_SESSION['auth'])){
-                    ?><li><a href="logout.php">Выйти из ситсемы</a></li><?php
+                    ?><li><a href = "logout.php">Выйти из ситсемы</a></li><?php
                 }else{?>
-                <li><a href="login.php">Автоотзация</a></li>
-                <li><a href="registration.php">Регистрация</a></li><?php
+                <li><a href = "login.php">Автоотзация</a></li>
+                <li><a href = "registration.php">Регистрация</a></li><?php
                 }
                 ?>
             </ul>
         </header>
         <main>
-            <?= $form_content ?><br>
+            <?= $formContent ?><br>
             <?php if(isset($_SESSION['message'])) echo $_SESSION['message']; unset($_SESSION['message'])?><br>
             <?= $content ?>
         </main>
